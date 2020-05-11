685c30ef04477b6c6704d407a613b11661f96261
Taylor Hutchison <seanthutchison@gmail.com>
2020-05-10 20:38:43 -0500

    Include two libraries: System.CommandLine Markdig

--- Ros.App/Program.cs
+++ Ros.App/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Markdig;
 
 namespace Ros.App
 {
@@ -6,7 +7,7 @@ namespace Ros.App
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
         }
     }
 }
